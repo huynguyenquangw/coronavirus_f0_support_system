4bd95d751a142ff3e59df9de06af9dc6872246e5
huynguyenquangw <s3697272@rmit.edu.vn>
2021-08-17 17:56:16 +0700

    [ADD] add city model ctrl router

---
+++ models/CityModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const CitySchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        abbreviation: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        postcode: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        districts:[
+            // {
+            //     type: mongoose.Schema.Types.ObjectId,
+            //     ref: "District"
+            // }
+        ]
+    }, 
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model("Cities", CitySchema)
