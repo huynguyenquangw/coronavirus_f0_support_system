921500e46f10f1f210cc13a75c75f1cb3f253f78
MaiVietCuong <s3682365@rmit.edu.vn>
2021-08-18 15:44:05 +0700

    update user router

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const userCtrl = require('../controllers/UserCtrl')
 const auth = require('../middleware/auth')
 
+//Authentication
 router.post('/register', userCtrl.register)
 
 router.post('/login', userCtrl.login)
@@ -10,8 +11,13 @@ router.get('/logout', userCtrl.logout)
 
 router.get('/refresh_token', userCtrl.refreshToken)
 
+//Get info of a patient
 router.get('/info', auth, userCtrl.getUser)
 
+//Get all patients
+router.get('/', userCtrl.getAllPatient)
 
+//Update patient by id
+router.put('/update/:id', auth, userCtrl.updatePatientByID)
 
 module.exports = router
