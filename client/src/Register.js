3c488e55969076a881b94344a14ac15ee694a96a
s3680599Huy <info.huytran99@gmail.com>
2021-08-21 18:54:08 +0700

    Register
    Huy

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,6 +1,63 @@
 import Navbar from './Navbar';
+import {useState } from 'react';
+import { useHistory } from 'react-router'
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+import { wait } from '@testing-library/dom';
  
+toast.configure()
  export default function Register(){
+    const history = useHistory()
+     const endPoint = "http://localhost:3000/user/register" 
+     const [confirmPass, setConfirmPass] = useState('');
+
+     const [user,setUser] = useState({
+        name:'',
+        email:'',
+        password: '',
+        district:'',
+        city:'',
+        phone:''
+     })
+      // chay Loading effect 
+     const [loading,setLoading]=useState(false)
+
+     const onChangeValue = e =>{
+         const {name,value} = e.target
+         setUser({...user,[name]:value})
+     }
+
+
+     const registerSubmit = async e => {
+         e.preventDefault()
+         if(user.password !== confirmPass){
+            toast('Password does not match !')
+         }else{
+            try {
+            setLoading(true)
+            await axios.post(endPoint,{...user})
+            localStorage.setItem('isLogin',true)
+            console.log(`User ${user.email} has been successfully registered !`)
+            toast(`User ${user.email} has been successfully registered !`)
+            
+            setLoading(false)
+            //Change url to profile page
+            // setTimeout( window.location.replace('/'), 10000)
+            history.push('/')
+   
+
+         } catch (error) {
+             console.log(error.response.data.msg)
+             toast(error.response.data.msg)
+            setLoading(false)
+
+         }  
+         }
+        
+     }
+     
  
  
     return (
@@ -18,19 +75,19 @@ import Navbar from './Navbar';
                          </div>
                          <br />
                     <div>
-                        <form action="">
-                            <input type="text" class="no1" id="fname" name="firstname" placeholder="First name.." />
-                            <input type="text"  class="no2" id="lname" name="lastname" placeholder="Last name.." />
+                        <form onSubmit={registerSubmit}>
+                            <input type="text" class="no3" id="name" name="name" placeholder="Name.." value = {user.name} onChange = {onChangeValue} />
+                            {/* <input type="text"  class="no2" id="lname" name="lastname" placeholder="Last name.." /> */}
                             <br />
-                            <input type="text"  class="no1" id="phone" name="phone"  placeholder="Phone.." />
-                            <input type="text"  class="no2" id="email" name="enail"  placeholder="Email.." />
+                            <input type="text"  class="no1" id="phone" name="phone"  placeholder="Phone.." value = {user.phone} onChange = {onChangeValue} />
+                            <input type="text"  class="no2" id="email" name="email"  placeholder="Email.."  value = {user.email} onChange = {onChangeValue}/>
                             <br />
-                            <input type="text"  class="no1" id="district" name="district"  placeholder="District.." />
-                            <input type="text"  class="no2" id="city" name="city"  placeholder="City.." />
+                            <input type="text"  class="no1" id="district" name="district"  placeholder="District.."value = {user.district} onChange = {onChangeValue} />
+                            <input type="text"  class="no2" id="city" name="city"  placeholder="City.." value = {user.city} onChange = {onChangeValue}/>
                             <br />
-                            <input type="text"  class="no3" id="password" name="password"  placeholder="Password.." />
+                            <input type="text"  class="no3" id="password" name="password"  placeholder="Password.."value = {user.password} onChange = {onChangeValue} />
                             <br />
-                            <input type="text"  class="no3" id="confirmPassword" name="confirmPassword"  placeholder="Confirm Password.." />
+                            <input type="text"  class="no3" id="confirmPassword" name="confirmPassword"  placeholder="Confirm Password.."  value={confirmPass} onChange={(e)=>setConfirmPass(e.target.value)}/>
                             <br />
                             <input type="submit" value="Register"/>
                         </form>
