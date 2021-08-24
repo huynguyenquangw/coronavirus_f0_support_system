e2258e9ded64be88147badce37c44b1bf4740e96
lukevo4 <trancongnguyenvo@gmail.com>
2021-08-24 22:18:52 +0700

    upload template structure

---
+++ client/src/containers/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Main from '../components/Main/Main'
+import Sidebar from '../components/Sidebar/Sidebar'
+
+function Dashboard() {
+    return (
+        <div>
+            <Sidebar/>
+            <Main/>
+        </div>
+    )
+}
+
+export default Dashboard
