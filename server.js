c379b65c203377662f1ed7721de5a1af1fd8d178
lukevo4 <trancongnguyenvo@gmail.com>
2021-08-31 14:57:51 +0700

    update backend

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const cookieParser = require('cookie-parser')
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+    credentials: true //Để bật cookie HTTP qua CORS
+}))
 app.use(fileUpload({
     useTempFiles: true
 }))
