86bc48c7eee9676596ecc671b2c779f29887fd90
s3680599Huy <info.huytran99@gmail.com>
2021-09-16 12:48:34 +0700

    CSSHealthFormDoc

--- client/src/doctor-components/Patient Chat/PatientsChat.js
+++ client/src/doctor-components/Patient Chat/PatientsChat.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { GlobalState } from '../../GlobalState'
 import { Link } from 'react-router-dom';
+import "./GetHealthPatient.css"
 
 function Patients(props) {
     const state = useContext(GlobalState)
@@ -13,43 +14,69 @@ function Patients(props) {
         "fever", "cough", "breathing", "sorethroat", "phlegm", "runnynose", "tiredness",
         "blocknose", "losssmell", "musclepain", "vaccinated", "covid", "status"
     ]
-
+    console.log(healthDeclares)
     return (
         <div>
-            <select name="filter" id="filter" value={filter} onChange={e => setFilter(e.target.value)}>
+            <label > Choose a conditions:</label>
+             
+            <select className="filterCondition" name="filter" id="filter" value={filter} onChange={e => setFilter(e.target.value)}>
                 {filterName.map((name, i) =>
                     <option key={i} value={name}>{name}</option>
                 )}
             </select>
 
             <div className="true-false">
+               <label className="radioStyle" > Check:</label>
                 {trueorfalse.map((ToF, i) => (
                     <label key={i} htmlFor={ToF}>
                         {ToF}
-                        <input type="radio" name="radiovalues" value={ToF}
+                        <input type="radio" name="radiovalues"  value={ToF}
                             onChange={e => setTrueOrFalse(e.target.value)}
                             checked={ToF === trueOrFalse} id={ToF}
                         />
                     </label>
                 ))}
             </div>
+            <br />
 
             <div className="display">
-                {healthDeclares.map(health => (
-                    <>
-                        {trueOrFalse === 'true' ? (
-                            <div key={health._id}>
-                                {health._id}
-                                {health.user_id?.name}
-                            </div>
-                        ) : (
-                            <Link to={`/doctor/prescriptions/medicine/${health._id}`}>
-                                {health._id}
-                                {health.user_id?.name}
-                            </Link>
-                        )}
-                    </>
-                ))}
+                <table>
+                    <tr>
+                        <th>
+                            Health Declaration ID
+                        </th>
+                        <th>
+                            Patient Name
+                        </th>
+                        <th> Vaccinated</th>
+                        <th> Covid</th>
+                        <th> Status</th>
+                    </tr>
+                    {healthDeclares.map(health => (
+                        <tr>
+                            {trueOrFalse === 'true' ? (
+
+                                <td key={health._id}> {health._id}</td>
+
+
+                            ) : (
+                                <Link to={`/doctor/prescriptions/medicine/${health._id}`}>
+                                    <td> {health._id}</td>
+
+                                </Link>
+                            )}
+                            <td key={health._id}>{health.user_id?.name}</td>
+                            <td> {health.vaccinated ? "true" : "false"}</td>
+                            <td> {health.covid ? "true" : "false"}</td>
+                            <td> {health.status ? "true" : "false"}</td>
+                        </tr>
+
+
+                    ))}
+
+                </table>
+
+
             </div>
         </div>
     )
